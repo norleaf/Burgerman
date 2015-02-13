1d76833784212f5a28f4b917f5c2adf03ecb3a07
Hans <norleaf@gmail.com>
2015-02-13 14:50:32 +0100

    helicopter.animspeed

--- Burgerman/Helicopter.cs
+++ Burgerman/Helicopter.cs
@@ -12,7 +12,10 @@ namespace Burgerman
         public Helicopter(Texture2D spriteTexture, Vector2 position) : base(spriteTexture, position)
         {
             Animation flying = new Animation(this);
+            flying.Delay = 100;
             flying.Frames.Add(new Rectangle(0, 0, 200, 74));
+            flying.Frames.Add(new Rectangle(200, 0, 200, 74));
+            flying.Frames.Add(new Rectangle(400, 0, 200, 74));
             flying.Frames.Add(new Rectangle(200, 0, 200, 74));
             
             setAnimation(flying);
